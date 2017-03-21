0e7f76e3292e21d0a33911a27ecff1b3e2c0f035
taylorkwan111 <707530848@qq.com>
2017-03-21 10:16:57 +0800

    精华部分加强

--- 百思不得姐/classes/Essence/Controller/DWEssenceViewController.m
+++ 百思不得姐/classes/Essence/Controller/DWEssenceViewController.m
@@ -8,14 +8,22 @@
 
 #import "DWEssenceViewController.h"
 #import "DWRecommendTagsViewController.h"
+#import "DWAllViewController.h"
+#import "DWVideoViewController.h"
+#import "DWVoiceViewController.h"
+#import "DWPictureViewController.h"
+#import "DWWordViewController.h"
 
-@interface DWEssenceViewController ()
+@interface DWEssenceViewController ()<UIScrollViewDelegate>
 //底部的红色指示器
 @property(nonatomic,weak)UIView *indicatorView;
 //当前选中的button
 @property(nonatomic,weak)UIButton *selectedButton;
 //当前选中的titleView
 @property(nonatomic,weak)UIView *titleView;
+//所有内容contentView
+@property(nonatomic,weak)UIScrollView *contentView;
+
 @end
 
 @implementation DWEssenceViewController
@@ -23,13 +31,34 @@
 - (void)viewDidLoad
 {
     [super viewDidLoad];
+    
     //设置导航栏内容
     [self setupNav];
+    //初始化子控制器
+    [self setupChildVces];
     //设置顶部的标签
     [self setupTitlesView];
     // 底部的srcollView
     [self setupContentView];
-
+    
+}
+-(void) setupChildVces
+{
+    DWAllViewController *all=[[DWAllViewController alloc]init];
+    [self addChildViewController:all];
+    
+    DWVideoViewController *video=[[DWVideoViewController alloc]init];
+    [self addChildViewController:video];
+    
+    DWVoiceViewController *voice=[[DWVoiceViewController alloc]init];
+    [self addChildViewController:voice];
+    
+    DWPictureViewController *picture=[[DWPictureViewController alloc]init];
+    [self addChildViewController:picture];
+    
+    DWWordViewController *word=[[DWWordViewController alloc]init];
+    [self addChildViewController:word];
+    
     
 }
 //设置顶部的标签
@@ -48,7 +77,7 @@
     indicatorView.backgroundColor=[UIColor redColor];
     indicatorView.height=2;
     indicatorView.y=titleView.height-indicatorView.height;
-    [titleView addSubview:indicatorView];
+   
     
     self.indicatorView=indicatorView;
     
@@ -60,6 +89,7 @@
     for (NSInteger i=0; i<5; i++)
     {
         UIButton *button=[[UIButton alloc]init];
+        button.tag=i;
         button.height=height;
         button.width=width;
         button.x=i*width;
@@ -75,6 +105,7 @@
             [self titleClick:button];
         }
     }
+    [titleView addSubview:indicatorView];
  
 }
 // 底部的srollView
@@ -82,31 +113,32 @@
 {
     self.automaticallyAdjustsScrollViewInsets=NO;
     UIScrollView * contentView=[[UIScrollView alloc]init];
-    contentView.backgroundColor=[UIColor blackColor];
+//    contentView.backgroundColor=[UIColor blackColor];
     contentView.frame=self.view.bounds;
-    // 设置内边距
-    CGFloat bottom=self.tabBarController.tabBar.height;
-    CGFloat top=CGRectGetMaxY(self.titleView.frame);
-    contentView.contentInset=UIEdgeInsetsMake(top, 0, bottom, 0);
-//    [contentView addSubview:[UIButton buttonWithType:UIButtonTypeContactAdd]];
-    
-//    contentView.width=self.view.width;
-//    contentView.y=99;
-//    contentView.height=self.view.height-self.tabBarController.tabBar.height-contentView.y;
+    self.contentView=contentView;
+    contentView.delegate=self;
+    contentView.pagingEnabled=YES;
     [self.view insertSubview:contentView atIndex:0];
-    contentView.contentSize=CGSizeMake(0, 800);
+    contentView.contentSize=CGSizeMake(contentView.width*self.childViewControllers.count, 0);
+    //添加第一个控制器的view
+    [self scrollViewDidEndScrollingAnimation:contentView];
     
 }
 -(void)titleClick:(UIButton *)button
 {
+    // 修改按钮状态
     self.selectedButton.enabled=YES;
     button.enabled=NO;
     self.selectedButton=button;
-        [UIView animateWithDuration:0.15 animations:^{
+    // 动画
+    [UIView animateWithDuration:0.15 animations:^{
             self.indicatorView.width=button.titleLabel.width;
-            self.indicatorView.centerX=button.centerX;
-
-        }];
+            self.indicatorView.centerX=button.centerX;}];
+    // 滚动
+    CGPoint offset=self.contentView.contentOffset;
+    offset.x= button.tag*self.contentView.width;
+    [self.contentView setContentOffset:offset animated:YES];
+    
 }
 //设置导航栏内容
 -(void)setupNav
@@ -122,6 +154,7 @@
 {
     DWRecommendTagsViewController *tags=[[DWRecommendTagsViewController alloc]init];
     [self.navigationController pushViewController:tags animated:YES];
+
 }
 
 /*
@@ -133,5 +166,33 @@
     // Pass the selected object to the new view controller.
 }
 */
+-(void)scrollViewDidEndScrollingAnimation:(UIScrollView *)scrollView
+{
+    // 添加子控制器
+    
+    //当前索引
+    NSInteger index=scrollView.contentOffset.x/scrollView.width;
+    //取出控制器
+    UITableViewController *vc=self.childViewControllers[index];
+    vc.view.x=scrollView.contentOffset.x;
+    vc.view.y=0;
+    vc.view.height=scrollView.height;
+    
+    //设置内边距
+    CGFloat bottom=self.tabBarController.tabBar.height;
+    CGFloat top=CGRectGetMaxY(self.titleView.frame);
+    vc.tableView.contentInset=UIEdgeInsetsMake(top, 0, bottom, 0);
+    //设置滚动条的内边距
+    vc.tableView.scrollIndicatorInsets=vc.tableView.contentInset;
+    [scrollView addSubview:vc.view];
+    
+}
+-(void)scrollViewDidEndDecelerating:(UIScrollView *)scrollView
+{
+    [self scrollViewDidEndScrollingAnimation:scrollView];
+    //点击按钮
+    NSInteger index=scrollView.contentOffset.x/scrollView.width;
+    [self titleClick:self.titleView.subviews[index]];
+}
 
 @end
---
+++ 百思不得姐/classes/Essence/Controller/DWVoiceViewController.h
@@ -0,0 +1,13 @@
+//
+//  DWVoiceViewController.h
+//  百思不得姐
+//
+//  Created by dw on 2017/3/16.
+//  Copyright © 2017年 dw. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface DWVoiceViewController : UITableViewController
+
+@end
